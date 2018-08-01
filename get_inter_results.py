53baf9471f3072f9d6d72a9e3f85c443dc6222d3
lampson.song <yuhao915@gmail.com>
2018-08-01 20:07:20 +0800

    add show inter results

--- get_inter_results.py
+++ get_inter_results.py
@@ -1,10 +1,11 @@
 # created by lampson.song @ 2018-08-01
-# show weights from frozed pb file
+# show weights / inter_resutls from frozed pb file
 
 # The used mobilenet_v2_1 model is downloaded from:
 # https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.0_224.tgz
 
 import tensorflow as tf
+import numpy as np
 
 restored_graph_def = tf.GraphDef()
 
@@ -14,15 +15,28 @@ def show_weights(node_name):
 
         for node in restored_graph_def.node:
             if node.name == node_name:
-                print(node)
-                weights = tf.convert_to_tensor(n.attr['value'].tensor)
-                print(weights.shape)
+                weights = tf.convert_to_tensor(node.attr['value'].tensor)
                 sess.run(tf.Print(weights,[weights],summarize=9)) # summarize is the number shown weights
+
+def show_inter_results(input_layer_name, inter_layer_name):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        graph = tf.get_default_graph()
+        input_layer = graph.get_tensor_by_name(input_layer_name)
+        inter_layer = graph.get_tensor_by_name(inter_layer_name)
+
+        input_data = np.ones((224,224,3))
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            out = sess.run(inter_layer, feed_dict={input_layer:[input_data]})
+            print(out)
 
 if __name__ == '__main__':
     graph = tf.get_default_graph()
 
-    frozen_graph = "./mobilenet_v2_1.0_224/mobilenet_v2_1.0_224_frozen.pb"
+    frozen_graph = "/home/lampson/2T_disk/model/tensorflow/mobilenet_v2_1.0_224/mobilenet_v2_1.0_224_frozen.pb"
 
     with tf.gfile.GFile(frozen_graph, "rb") as f:
             restored_graph_def = tf.GraphDef()
@@ -38,4 +52,6 @@ if __name__ == '__main__':
     # list all node names
     for n in restored_graph_def.node: 
         print(n.name)
+
     show_weights("Const_145")
+    show_inter_results("input:0","Const_100:0")
